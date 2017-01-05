bd411bbeee1674439ebaf4531dc2b922c62ebc88
Kai Huebl <kai@huebl-sgh.de>
2017-01-05 22:38:49 +0100

    added opc ua client modul

---
+++ src/OpcUaClientModul/Library/Library.cpp
@@ -0,0 +1,47 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaNodeSetModul/Library/Library.h"
+
+#include <QMainWindow>
+#include <QIcon>
+
+namespace OpcUaNodeSet
+{
+
+	Library::Library(void)
+	: OpcUaGui::ModulInterface()
+	{
+	}
+
+	Library::~Library(void)
+	{
+	}
+
+	QIcon*
+	Library::modulIcon(void)
+	{
+		return new QIcon(":images/OpcUaClient.png");
+	}
+
+}
+
+extern "C" MYSHAREDLIB_EXPORT void  init(OpcUaGui::ModulInterface** modulInterface) {
+    *modulInterface = new OpcUaNodeSet::Library();
+}
+
+
---
+++ src/OpcUaClientModul/Library/Library.h
@@ -0,0 +1,64 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaNodeSetModul_Library_h__
+#define __OpcUaNodeSetModul_Library_h__
+
+#include <QObject>
+
+#include "OpcUaGui/ModulInterface/ModulInterface.h"
+
+namespace OpcUaNodeSet
+{
+
+	class MYSHAREDLIB_EXPORT Library
+	: public OpcUaGui::ModulInterface
+	{
+	  Q_OBJECT
+
+	  public:
+		Library(void);
+		virtual ~Library(void);
+
+		//- ModulInterface ----------------------------------------------------
+		virtual QIcon* modulIcon(void);
+		//- ModulInterface ----------------------------------------------------
+
+	  private:
+	};
+
+
+	class MYSHAREDLIB_EXPORT xxxx
+	: public QObject
+	{
+		Q_OBJECT
+
+	  public:
+		xxxx(void) {}
+		virtual ~xxxx(void) {}
+
+      public slots:
+        void startupLibrary(void) {}
+        void shutdownLibrary(void) {}
+
+      signals:
+
+	};
+
+}
+
+#endif
