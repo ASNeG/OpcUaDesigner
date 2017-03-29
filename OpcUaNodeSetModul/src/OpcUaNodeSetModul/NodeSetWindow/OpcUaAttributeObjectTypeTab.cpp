a763307b90feac4e69dedbebe6dc3e9424051596
Kai Huebl <kai@huebl-sgh.de>
2017-03-29 10:01:46 +0200

    added attribute object type tab class

---
+++ OpcUaNodeSetModul/src/OpcUaNodeSetModul/NodeSetWindow/OpcUaAttributeObjectTypeTab.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <QVBoxLayout>
+
+#include "OpcUaNodeSetModul/NodeSetWindow/OpcUaAttributeObjectTypeTab.h"
+
+#include <QLabel>
+
+namespace OpcUaNodeSet
+{
+
+
+	OpcUaAttributeObjectTypeTab::OpcUaAttributeObjectTypeTab(QWidget* parent)
+	: QWidget()
+	{
+		// create opc ua attribute base tab
+		QLabel* label = new QLabel("todo");
+
+		// show opc ua attriubute base tab
+		QVBoxLayout* layout_ = new QVBoxLayout();
+		layout_->addWidget(label);
+		setLayout(layout_);
+	}
+
+	OpcUaAttributeObjectTypeTab::~OpcUaAttributeObjectTypeTab(void)
+	{
+	}
+
+}
+
+
