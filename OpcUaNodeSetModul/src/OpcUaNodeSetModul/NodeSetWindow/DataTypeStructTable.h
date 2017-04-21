d9455ecc46d147a1a3f739cffab76a689537d025
Kai Huebl <kai@huebl-sgh.de>
2017-04-21 13:51:27 +0200

    added new class DataTypeStructTable

---
+++ OpcUaNodeSetModul/src/OpcUaNodeSetModul/NodeSetWindow/DataTypeStructTable.h
@@ -0,0 +1,44 @@
+/*
+   Copyright 2017 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaNodeSet_DataTypeStructTable_h__
+#define __OpcUaNodeSet_DataTypeStructTable_h__
+
+#include <QWidget>
+
+#include "OpcUaNodeSetModul/NodeSetWindow/NodeInfo.h"
+
+namespace OpcUaNodeSet
+{
+
+	class DataTypeStructTable
+	: public QWidget
+	{
+		Q_OBJECT
+
+	  public:
+		DataTypeStructTable(QWidget* parent = 0);
+		virtual ~DataTypeStructTable(void);
+
+		void nodeChange(NodeInfo* nodeInfo);
+
+	  private:
+	};
+
+}
+
+#endif
